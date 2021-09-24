b51260ca4b60f367d79bc9fbfa4df1c9044377f4
checodaniel04 <checo04@gmail.com>
2021-09-23 19:50:42 -0500

    sd

--- resources/views/admin/personas/create.blade.php
+++ resources/views/admin/personas/create.blade.php
@@ -142,3 +142,41 @@
     
         </div>
 @stop
+<script>
+  
+
+    document.getElementById("file").addEventListener('change', cambiarImagen);
+
+        function cambiarImagen(event){
+            var file = event.target.files[0];
+
+            var reader = new FileReader();
+            reader.onload = (event) => {
+                document.getElementById("picture").setAttribute('src', event.target.result);
+            };
+
+            reader.readAsDataURL(file);
+        }
+
+</script>
+
+@stop
+
+@section('css')
+<style>
+.image-wrapper{
+    position:relative;
+    padding-bottom:56.25%;
+}
+
+.image-wrapper img{
+    position:absolute;
+    object-fit:cover;
+    width:100%;
+    height: 100%;
+}
+</style>
+
+    
+@endsection
+
